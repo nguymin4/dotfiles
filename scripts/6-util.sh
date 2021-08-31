1f66bb50246df2a48dd49fad687089e009fd9c03
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2021-08-31 11:12:25 +0300

    feat: update scripts

--- scripts/6-util.sh
+++ scripts/6-util.sh
@@ -7,8 +7,8 @@ curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 # Heroku
-curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install heroku-accounts
+# curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+# heroku plugins:install heroku-accounts
 
 sudo add-apt-repository -y ppa:bamboo-engine/ibus-bamboo
 sudo add-apt-repository -y ppa:mmstick76/alacritty
