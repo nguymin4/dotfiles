6ecbd093e874d15239d20d485ea09417b4719cb2
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-01-14 12:13:26 +0200

    update: android emulator related config

--- scripts/7-optional.sh
+++ scripts/7-optional.sh
@@ -1,5 +1,29 @@
+#!/bin/bash
+
+set -o errexit
+
+ANDROID_SDK_ROOT=$HOME/Programs/android
+CLI_TOOL_DOWNLOAD_LINK=$(curl -s https://developer.android.com/studio\#command-tools | grep -o 'https.*commandlinetools-linux.*\.zip')
+echo "Downloading $CLI_TOOL_DOWNLOAD_LINK..."
+mkdir -p $ANDROID_SDK_ROOT && cd $ANDROID_SDK_ROOT
+curl -L $CLI_TOOL_DOWNLOAD_LINK -o commandlinetools.zip
+
+echo "Setting up Android command line tools..."
+unzip commandlinetools.zip && rm commandlinetools.zip
+mv cmdline-tools latest
+mkdir cmdline-tools && mv latest cmdline-tools/
+
+cat <<-'EOH' >> $HOME/.path
+export ANDROID_SDK_ROOT=$HOME/Programs/android
+export PATH=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin:$PATH
+export PATH=$ANDROID_SDK_ROOT/tools:$PATH
+export PATH=$ANDROID_SDK_ROOT/emulator:$PATH
+
+EOH
+
+source $HOME/.path
+sdkmanager --install "emulator" "platform-tools" "platforms;android-29" "build-tools;29.0.2" "system-images;android-29;google_apis_playstore;x86_64"
+
+avdmanager create avd -n pixel-android -k "system-images;android-29;google_apis_playstore;x86_64"
+
 sudo apt install watchman
-
-sdkmanager --install "emulator" "platform-tools" "platforms;android-29" "build-tools;29.0.2" "system-images;android-29;default;x86_64"
-
-avdmanager create avd -n meru -k "system-images;android-29;default;x86_64"
