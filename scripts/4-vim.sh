9f5f2b59b449e03ef55c32a8577caa3c95c30e83
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-02-21 00:53:47 +0200

    script: add latex dependencies

--- scripts/4-vim.sh
+++ scripts/4-vim.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 sudo add-apt-repository -y ppa:jonathonf/vim-daily
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt install -y vim-gtk3 neovim python3-neovim libbz2-dev libreadline-dev libsqlite3-dev libssl-dev libffi-dev liblzma-dev tk-dev texlive latexmk
+sudo apt install -y vim-gtk3 neovim python3-neovim libbz2-dev libreadline-dev libsqlite3-dev libssl-dev libffi-dev liblzma-dev tk-dev texlive textlive-luatex textlive-pictures texlive-latex-extra latexmk
 
 function checkout_latest_tag() {
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
@@ -19,6 +19,7 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zprofile
 echo '[ -s "$PYENV_ROOT/bin/pyenv" ] && eval "$(pyenv init --path)"' >> ~/.zprofile
 pip install -U pynvim
 
+# nvm
 rm -rf ~/.nvm && git clone https://github.com/nvm-sh/nvm.git ~/.nvm
 cd ~/.nvm && checkout_latest_tag
 
