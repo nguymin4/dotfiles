4ff576f2c58b8e139e5ab2746695d4a8e22a8511
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-07-18 16:50:18 +0000

    feat: support backup dotfiles in VM

--- backup.sh
+++ backup.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
-set -o errexit
+set -euo pipefail
+
+print_usage() {
+    echo 'Usage: bash backup.sh [--config $config_file] --target $target_folder'
+    exit 1
+}
+
+CONFIG_FILE='rsync-dotfiles'
+TARGET_FOLDER='dotfiles'
+
+while getopts ':-:' flag; do
+  case "${flag}" in
+    -)
+      case "${OPTARG}" in
+        config)
+          CONFIG_FILE="${!OPTIND}";
+          OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        target)
+          TARGET_FOLDER="${!OPTIND}";
+          OPTIND=$(( $OPTIND + 1 ))
+          ;;
+      esac;;
+    *) print_usage ;;
+  esac
+done
 
 SOURCE_FOLDER=$HOME
-TARGET_FOLDER=$(realpath -s ${1:-dotfiles})
-RSYNC_LIST="$(dirname $(realpath -s $0))/rsync-dotfiles"
+TARGET_FOLDER=$(realpath -s $TARGET_FOLDER)
+RSYNC_LIST="$(dirname $(realpath -s $0))/$CONFIG_FILE"
 
 rsync -avi --recursive --relative --delete --exclude='*.swp' --files-from="$RSYNC_LIST" $SOURCE_FOLDER $TARGET_FOLDER
 
--- install.sh
+++ install.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 
-set -o errexit
+set -euo pipefail
 
-if [ -z $1 ]; then
-  echo "Error: Missing target folder"
-  exit 1
-else
-  rsync -avi dotfiles/ $1
+print_usage() {
+    echo 'Usage: bash install.sh [--src $source_folder] --target $target_folder'
+    exit 1
+}
+
+SRC_FOLDER='dotfiles'
+TARGET_FOLDER=''
+
+while getopts ':-:' flag; do
+  case "${flag}" in
+    -)
+      case "${OPTARG}" in
+        src)
+          SRC_FOLDER="${!OPTIND}";
+          OPTIND=$(( $OPTIND + 1 ))
+          ;;
+        target)
+          TARGET_FOLDER="${!OPTIND}";
+          OPTIND=$(( $OPTIND + 1 ))
+          ;;
+      esac;;
+    *) print_usage ;;
+  esac
+done
+
+if [ ! "$TARGET_FOLDER" ]
+then
+    print_usage
 fi
+
+rsync -avi $SRC_FOLDER/ $TARGET_FOLDER
+echo "Synced dotfiles from $SRC_FOLDER to $TARGET_FOLDER"
