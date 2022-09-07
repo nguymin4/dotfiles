e322179122dadf683ffa8841dc7d57b766fc51f8
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-09-07 04:15:08 +0300

    i3: use polybar

--- scripts/20-i3-gaps.sh
+++ scripts/20-i3-gaps.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 sudo apt install -y ninja-build meson libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
 
 # i3-gaps
-sudo apt install -y --no-install-recommends i3status i3blocks
+sudo apt install -y --no-install-recommends i3status i3blocks polybar
 git clone https://github.com/Airblader/i3.git ~/Programs/i3
 cd ~/Programs/i3
 mkdir -p build && cd build
