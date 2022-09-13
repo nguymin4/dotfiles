02cb727dfaf74ff44ac379c988a196a1ccc8a7b3
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-09-13 19:28:22 +0300

    refactor: installation script

--- scripts/22-theme.sh
+++ scripts/22-theme.sh
@@ -6,9 +6,7 @@ set -euo pipefail
 echo "deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_$(lsb_release -rs)/ /" | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
 curl -fsSL "https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_$(lsb_release -rs)/Release.key" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
 
-sudo apt update && sudo apt install -y variety dunst lm-sensors albert thunar thunar-archive-plugin
-
-xdg-mime default thunar.desktop inode/directory
+sudo apt update && sudo apt install -y variety dunst lm-sensors albert
 
 # GTK
 sudo apt install -y lxappearance arc-theme breeze-cursor-theme
