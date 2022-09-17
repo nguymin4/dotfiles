4c17b41b675facddf8421198545c7845fcdc60f3
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-09-17 20:35:11 +0300

    sway: setup kde-auth-agent for gui application

--- scripts/21-sway.sh
+++ scripts/21-sway.sh
@@ -10,3 +10,6 @@ chmod u+x -R ~/.config/waybar/blocks
 
 # Screen sharing via browser
 sudo apt install -y xdg-desktop-portal-wlr wireplumber
+
+# Allow GUI application authentication as root
+sudo apt install polkit-kde-agent-1
