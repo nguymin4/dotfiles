0a9c5da59e5d75465613154f142decaafdbc953a
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-09-23 20:21:33 +0300

    vim: use trash-cli to delete file

--- scripts/11-vim.sh
+++ scripts/11-vim.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 sudo add-apt-repository -y ppa:jonathonf/vim-daily
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt install -y ack vim-gtk3 neovim libbz2-dev libreadline-dev libsqlite3-dev libssl-dev libffi-dev liblzma-dev tk-dev texlive texlive-luatex texlive-pictures texlive-latex-extra latexmk
+sudo apt install -y ack vim-gtk3 neovim trash-cli libbz2-dev libreadline-dev libsqlite3-dev libssl-dev libffi-dev liblzma-dev tk-dev texlive texlive-luatex texlive-pictures texlive-latex-extra latexmk
 
 pip install -U pynvim
 zsh -c "source ~/.zshrc && vim +PlugInstall +qall"
