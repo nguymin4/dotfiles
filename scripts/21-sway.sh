5a577e7d4e9f81d4d84c9458192f091806e359b3
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-10-10 01:31:44 +0300

    script: support bluetooth speaker in sway

--- scripts/21-sway.sh
+++ scripts/21-sway.sh
@@ -11,6 +11,10 @@ chmod u+x -R ~/.config/waybar/blocks
 # Screen sharing via browser
 sudo apt install -y xdg-desktop-portal-wlr wireplumber
 
+# Bluetooth speaker
+sudo apt install -y libspa-0.2-bluetooth pipewire-audio-client-libraries
+# sudo apt remove pulseaudio-module-bluetooth
+
 # fcitx5
 sudo apt install -y fcitx5 fcitx5-unikey
 fcitx5 -dr > /dev/null
