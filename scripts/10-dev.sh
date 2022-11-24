4bac279df116320bf2114633b13b7fc5ab159b6c
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-11-24 13:32:54 +0200

    script: setup fnm to replace nvm

--- scripts/10-dev.sh
+++ scripts/10-dev.sh
@@ -2,16 +2,22 @@
 
 set -euo pipefail
 
-sudo apt install watchman
-
 function get_latest_tag_from_github() {
   GITHUB_REPO_URL="https://github.com/$1"
   curl -s "${GITHUB_REPO_URL}/tags" | grep -w releases | grep -o '[0-9]\.[0-9]*\.[0-9-]*' | grep -v - | head -n 1
 }
 
-# nvm
-rm -rf ~/.nvm && git clone https://github.com/nvm-sh/nvm.git ~/.nvm
-zsh -c "source ~/.zshrc && nvm install --lts && npm install -g yarn"
+# fnm
+FNM_VERSION=$(get_latest_tag_from_github 'Schniz/fnm')
+curl -L "https://github.com/Schniz/fnm/releases/download/v${FNM_VERSION}/fnm-linux.zip" -o ~/.local/bin/fnm.zip
+unzip -o ~/.local/bin/fnm.zip -d ~/.local/bin
+chmod u+x ~/.local/bin/fnm
+rm ~/.local/bin/fnm.zip
+fnm install --lts && npm install -g yarn
+
+# # nvm
+# rm -rf ~/.nvm && git clone https://github.com/nvm-sh/nvm.git ~/.nvm
+# zsh -c "source ~/.zshrc && nvm install --lts && npm install -g yarn"
 
 # pyenv
 rm -rf ~/.pyenv && git clone https://github.com/pyenv/pyenv.git ~/.pyenv
@@ -33,7 +39,7 @@ ln -s ~/.tfenv/bin/* ~/.local/bin
 sudo apt install -y libnss3-tools
 MKCERT_VERSION=$(get_latest_tag_from_github 'FiloSottile/mkcert')
 curl -L "https://github.com/FiloSottile/mkcert/releases/download/v${MKCERT_VERSION}/mkcert-v${MKCERT_VERSION}-linux-amd64" -o ~/.local/bin/mkcert
-sudo chmod u+x ~/.local/bin/mkcert
+chmod u+x ~/.local/bin/mkcert
 # ~/.local/bin/mkcert install
 
 # PgAdmin4
@@ -44,3 +50,5 @@ sudo chmod u+x ~/.local/bin/mkcert
 # Heroku
 # curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 # heroku plugins:install heroku-accounts
+
+sudo apt install watchman
