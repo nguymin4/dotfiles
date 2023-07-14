94b8d759a0a279675ae5cf696b2b0def3c64c6d6
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-07-14 20:31:27 +0300

    feat(vim): tweak nvim tree settings

--- dotfiles/.config/nvim/lua/custom/nvim-tree.lua
+++ dotfiles/.config/nvim/lua/custom/nvim-tree.lua
@@ -18,12 +18,14 @@ local function on_attach(bufnr)
 
   -- remove some default keybindings to avoid conflicts
   vim.keymap.del('n', '<C-k>', { buffer = bufnr })
+  vim.keymap.del('n', 'e', { buffer = bufnr })
+  vim.keymap.del('n', 'd', { buffer = bufnr })
 
   -- custom mappings
   vim.keymap.set('n', 'u', api.tree.change_root_to_parent, opts('Up'))
   vim.keymap.set('n', 'cd', api.tree.change_root_to_node, opts('CD'))
-  vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
-  vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 'i', api.node.open.horizontal, opts('Open: Horizontal Split'))
+  vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'))
   vim.keymap.set('n', 'dd', api.fs.cut, opts('Cut'))
   vim.keymap.set('n', 'yy', api.fs.copy.node, opts('Copy'))
   vim.keymap.set('n', 'yn', api.fs.copy.filename, opts('Copy Name'))
@@ -48,7 +50,7 @@ nvim_tree.setup({
     },
   },
   filters = {
-    dotfiles = true,
+    dotfiles = false,
   },
   git = {
     ignore = false,
