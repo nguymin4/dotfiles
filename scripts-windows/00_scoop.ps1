c3cc8afbeac126ed43d38f555d983469d67f502e
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-08-10 02:49:30 +0300

    feat: add windows setup scripts

---
+++ scripts-windows/00_scoop.ps1
@@ -0,0 +1,14 @@
+Invoke-RestMethod get.scoop.sh | Invoke-Expression
+
+scoop bucket add extras
+scoop bucket add versions
+
+# CLI
+scoop install git neovim starship ack wget fnm fd bat pwsh refreshenv msys2
+
+# UI
+scoop install vscode alacritty wezterm glazewm flow-launcher autohotkey
+
+# Fonts
+scoop bucket add nerd-fonts
+scoop install nerd-fonts/Ubuntu-NF nerd-fonts/UbuntuMono-NF-Mono
---
+++ scripts-windows/04_mingw64.sh
@@ -0,0 +1,1 @@
+pacman -Ss git zsh fish
