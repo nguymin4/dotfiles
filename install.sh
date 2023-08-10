a813f3536331d3325cd13b3d4bb1d8eed151ca4d
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-08-10 14:36:10 +0300

    feat: support multiple platforms in install.sh

--- install.sh
+++ install.sh
@@ -2,35 +2,61 @@
 
 set -euo pipefail
 
-print_usage() {
-    echo 'Usage: bash install.sh [--src $source_folder] --target $target_folder'
-    exit 1
+help() {
+    cat <<- EOF
+Usage: bash install.sh [OPTIONS] --target \$target_folder
+--help        Show this message
+--linux
+--vm
+--windows
+EOF
 }
 
-SRC_FOLDER='dotfiles'
+TYPE=''
 TARGET_FOLDER=''
 
 while getopts ':-:' flag; do
   case "${flag}" in
     -)
       case "${OPTARG}" in
-        src)
-          SRC_FOLDER="${!OPTIND}";
-          OPTIND=$(( $OPTIND + 1 ))
+        linux|vm|windows)
+          TYPE=$OPTARG
           ;;
         target)
           TARGET_FOLDER="${!OPTIND}";
           OPTIND=$(( $OPTIND + 1 ))
           ;;
+        help)
+          help
+          exit 0
+          ;;
       esac;;
-    *) print_usage ;;
+    *)
+      echo "unknown option: $flag"
+      help
+      exit 1
+      ;;
   esac
 done
 
-if [ ! "$TARGET_FOLDER" ]
+if [[ -z $TARGET_FOLDER || -z $TYPE ]] 
 then
-    print_usage
+    help
+    exit 1
 fi
 
-rsync -avi $SRC_FOLDER/ $TARGET_FOLDER
-echo "Synced dotfiles from $SRC_FOLDER to $TARGET_FOLDER"
+function run_rsync() {
+  source_folder=$1
+  rsync -avi $source_folder/ $TARGET_FOLDER
+  echo "Synced dotfiles from $source_folder to $TARGET_FOLDER"
+}
+
+case $TYPE in
+  linux)
+    run_rsync dotfiles ;;
+  vm)
+    run_rsync dotfiles-vm ;;
+  windows)
+    run_rsync dotfiles-windows ;;
+esac
+
