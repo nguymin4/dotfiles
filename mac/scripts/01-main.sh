620f2f58bb5ae95b54701b51224122a2b5e4d86b
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-08-14 18:53:37 +0300

    feat(yabai): update yabai and sketchybar

--- mac/scripts/01-main.sh
+++ mac/scripts/01-main.sh
@@ -8,8 +8,9 @@ sudo ln -sf /opt/homebrew/bin/python3 /opt/homebrew/bin/python
 sudo ln -sf /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
 
 # Check cloud components installation
-brew install --cask alacritty google-cloud-sdk visual-studio-code
+brew install --cask alacritty google-cloud-sdk spaceid stats visual-studio-code
 xattr -rd com.apple.quarantine /Applications/Alacritty.app
+xattr -rd com.apple.quarantine /Applications/SpaceId.app
 
 # Fonts
 brew tap homebrew/cask-fonts
