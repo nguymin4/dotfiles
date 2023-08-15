cffb946ba291ddbb4972c45fdc255256bea349f8
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-08-16 01:12:41 +0300

    feat(mac): install ueli

--- mac/scripts/01-main.sh
+++ mac/scripts/01-main.sh
@@ -8,9 +8,10 @@ sudo ln -sf /opt/homebrew/bin/python3 /opt/homebrew/bin/python
 sudo ln -sf /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
 
 # Check cloud components installation
-brew install --cask alacritty google-cloud-sdk spaceid stats visual-studio-code
+brew install --cask alacritty google-cloud-sdk spaceid stats ueli visual-studio-code
 xattr -rd com.apple.quarantine /Applications/Alacritty.app
 xattr -rd com.apple.quarantine /Applications/SpaceId.app
+xattr -rd com.apple.quarantine /Applications/ueli.app
 
 # Fonts
 brew tap homebrew/cask-fonts
