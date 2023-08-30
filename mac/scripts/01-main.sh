6d9b759e99dd7a99b1fa06f61b9d3c81895da7d5
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-08-31 01:29:02 +0300

    feat(mac): replace ueli with alfred

--- mac/scripts/01-main.sh
+++ mac/scripts/01-main.sh
@@ -10,10 +10,9 @@ sudo ln -sf /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
 sudo ln -sf /opt/homebrew/bin/python3 /opt/homebrew/bin/python
 
 # Check cloud components installation
-brew install --cask alacritty google-cloud-sdk spaceid stats ueli visual-studio-code
+brew install --cask alacritty alfred google-cloud-sdk spaceid stats visual-studio-code
 xattr -rd com.apple.quarantine /Applications/Alacritty.app
 xattr -rd com.apple.quarantine /Applications/SpaceId.app
-xattr -rd com.apple.quarantine /Applications/ueli.app
 
 # Fonts
 brew tap homebrew/cask-fonts
