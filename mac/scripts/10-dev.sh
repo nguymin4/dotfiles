e584c3e81227bd75aa2425f78eae5ec48391a7a3
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2023-10-03 14:51:10 +0300

    feat(mac): install docker buildx

--- mac/scripts/10-dev.sh
+++ mac/scripts/10-dev.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 
 # docker
 function install_docker() {
-  brew install colima docker docker-compose kompose kubectl minikube
+  brew install colima docker docker-buildx docker-compose kompose kubectl minikube
   minikube config set driver docker
 
-  # docker compose as plugin
+  # docker plugins
   mkdir -p ~/.docker/cli-plugins
+  ln -sfn "$HOMEBREW_PREFIX/opt/docker-buildx/bin/docker-buildx" ~/.docker/cli-plugins/docker-buildx
   ln -sfn "$HOMEBREW_PREFIX/opt/docker-compose/bin/docker-compose" ~/.docker/cli-plugins/docker-compose
 }
 
