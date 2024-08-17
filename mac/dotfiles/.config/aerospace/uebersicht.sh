4a9954fbe9ab774e55d8901801652fb88f6e60a1
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2024-08-18 00:14:44 +0300

    feat(mac): aerospace communicate with simple-bar via http

--- mac/dotfiles/.config/aerospace/uebersicht.sh
+++ mac/dotfiles/.config/aerospace/uebersicht.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-osascript -e 'tell application id "tracesOf.Uebersicht" to refresh widget id "simple-bar-index-jsx"'
+pgrep -x simple-bar-server > /dev/null
+
+if [ $? -eq 1 ]; then
+  cd ~/.config/uebersicht/simple-bar-server || exit
+  source .env/bin/activate
+  nohup python main.py 1>/dev/null 2>&1 &
+  sleep 1
+fi
+
+port=7776
+kind=$1
+
+if [[ -z $kind ]]; then
+  echo "Usage: ./uebersicht.sh <kind>"
+  exit 0
+elif [[ $kind == "spaces" ]]; then
+  payload="{\"focusedSpace\": $AEROSPACE_FOCUSED_WORKSPACE, \"prevSpace\": $AEROSPACE_FOCUSED_WORKSPACE}"
+else
+  payload=$(aerospace list-windows --focused \
+    --format '{"windowId": "%{window-id}", "appName": "%{app-name}", "workspace": %{workspace} }'
+  )
+fi
+
+
+url="http://localhost:$port/aerospace/$kind/refresh"
+curl -X POST "$url" -H "Content-Type: application/json" -d "$payload"
--- mac/scripts/22-uebersicht.sh
+++ mac/scripts/22-uebersicht.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 brew install --cask ubersicht
 
-WIDGET_FOLDER="$HOME/.config/uebersicht/widgets"
+UEBERSICHT_FOLDER="$HOME/.config/uebersicht"
+WIDGET_FOLDER="$UEBERSICHT_FOLDER/widgets"
 mkdir -p "$WIDGET_FOLDER"
 git clone https://github.com/nguymin4/simple-bar.git "$WIDGET_FOLDER/simple-bar"
+git clone https://github.com/nguymin4/simple-bar-server.git "$UEBERSICHT_FOLDER"
