397048b0e79133546d4a7bd2eb45567298854221
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2024-09-08 18:23:23 +0300

    refactor(aerospace): use simple-bar-server-go

--- mac/dotfiles/.config/aerospace/uebersicht.sh
+++ mac/dotfiles/.config/aerospace/uebersicht.sh
@@ -3,10 +3,9 @@
 pgrep -x simple-bar-server > /dev/null
 
 if [ $? -eq 1 ]; then
-  cd ~/.config/uebersicht/simple-bar-server || exit
-  source .env/bin/activate
-  nohup python main.py 1>/dev/null 2>&1 &
-  sleep 1
+  cd ~/.config/uebersicht/simple-bar-server-go || exit
+  nohup ./bin/simple-bar-server . 1>/dev/null 2>&1 &
+  exit 1
 fi
 
 port=7776
@@ -16,7 +15,7 @@ if [[ -z $kind ]]; then
   echo "Usage: ./uebersicht.sh <kind>"
   exit 0
 elif [[ $kind == "spaces" ]]; then
-  payload="{\"focusedSpace\": $AEROSPACE_FOCUSED_WORKSPACE, \"prevSpace\": $AEROSPACE_FOCUSED_WORKSPACE}"
+  payload="{\"focusedSpace\": $AEROSPACE_FOCUSED_WORKSPACE, \"prevSpace\": $AEROSPACE_PREV_WORKSPACE}"
 else
   payload=$(aerospace list-windows --focused \
     --format '{"windowId": "%{window-id}", "appName": "%{app-name}", "workspace": %{workspace} }'
