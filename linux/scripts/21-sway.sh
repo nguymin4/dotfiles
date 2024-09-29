07839c0e3d2284815de19a9e1650bcbfd43be146
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2024-09-29 15:26:51 +0300

    feat(linux): support sway with intel-nvidia hybrid setup

--- linux/scripts/21-sway.sh
+++ linux/scripts/21-sway.sh
@@ -3,9 +3,6 @@
 set -euo pipefail
 
 sudo apt install -y sway swaylock swayidle waybar slurp grimshot wl-clipboard wlr-randr kanshi
-
-pip install --user swaytools
-
 chmod u+x -R ~/.config/waybar/blocks
 
 # Screen sharing via browser
@@ -18,3 +15,18 @@ sudo apt install -y libspa-0.2-bluetooth pipewire-audio-client-libraries
 # fcitx5
 sudo apt install -y fcitx5 fcitx5-unikey
 fcitx5 -dr > /dev/null
+
+# Multi-GPU setup: Intel-Nvidia
+# Use integrated (Intel) card for sway as Nvidia is not officially supported
+# Node name `card1` is chosen by cross-checking /dev/dri/by-path and `lspci`
+sudo tee /usr/share/wayland-sessions/sway-nvidia.desktop > /dev/null <<- 'EOH'
+[Desktop Entry]
+Name=Sway - Nvidia
+Comment=An i3-compatible Wayland compositor
+Exec=env WLR_DRM_DEVICES=/dev/dri/card1 sway --unsupported-gpu
+Type=Application
+DesktopNames=sway
+EOH
+
+# swayinfo similar to xprop
+pip install --user swaytools
