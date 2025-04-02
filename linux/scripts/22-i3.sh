7bbf209a30d6bf6b948b7d0ca7a120b202d61ae9
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2025-04-02 23:05:38 +0300

    feat(script): update i3 installation

--- linux/scripts/22-i3.sh
+++ linux/scripts/22-i3.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 # i3 - https://i3wm.org/docs/repositories.html
-/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2024.03.04_all.deb /tmp/keyring.deb SHA256:f9bb4340b5ce0ded29b7e014ee9ce788006e9bbfe31e96c09b2118ab91fca734
+/usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2025.03.09_all.deb /tmp/keyring.deb SHA256:2c2601e6053d5c68c2c60bcd088fa9797acec5f285151d46de9c830aaba6173c
 sudo dpkg -i /tmp/keyring.deb
 echo \
-  "deb [arch=$(dpkg --print-architecture)] http://debian.sur5r.net/i3/ $(lsb_release -cs) universe" \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/sur5r-keyring.gpg] http://debian.sur5r.net/i3/ $(lsb_release -cs) universe" \
   | sudo tee /etc/apt/sources.list.d/sur5r-i3.list > /dev/null
 sudo apt update
 sudo apt install -y --no-install-recommends i3 i3blocks polybar
