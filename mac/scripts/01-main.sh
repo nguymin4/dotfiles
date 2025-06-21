601da1b3482765b4225b931019ead815de54df37
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2025-06-21 14:02:29 +0300

    feat(mac): update brew installation script

--- mac/scripts/01-main.sh
+++ mac/scripts/01-main.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-brew install coreutils fastfetch fd gnu-sed grep htop jq nmap rg tig wget yazi
+# CLI
+brew install coreutils fastfetch fd gnu-sed grep htop jq nmap rg tig watch wget yazi
 
+# Python
 sudo ln -sf /opt/homebrew/bin/pip3 /opt/homebrew/bin/pip
 sudo ln -sf /opt/homebrew/bin/python3 /opt/homebrew/bin/python
 
-brew install --cask alacritty alfred spaceid stats visual-studio-code wezterm
+# Cask
+brew install --cask alacritty alfred visual-studio-code wezterm
 xattr -rd com.apple.quarantine /Applications/Alacritty.app
+# brew install --cask spaceid stats 
 
 # Fonts
 brew install font-ubuntu-nerd-font font-ubuntu-mono-nerd-font font-codicon
