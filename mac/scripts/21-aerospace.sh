034204252bc4af3ba2198bcaf478a1f8fc063760
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2026-09-18 11:35:13 +0300

    refactor(mac): remove yabai configs

--- mac/scripts/21-aerospace.sh
+++ mac/scripts/21-aerospace.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 brew install --cask nikitabobko/tap/aerospace
-brew install koekeishiya/formulae/skhd
+brew trust asmvik/formulae/skhd
+brew install asmvik/formulae/skhd
 
 # Detect app-id to use in aerospace config
 # osascript -e 'id of app "Microsoft Teams"'
